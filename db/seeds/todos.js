cf6cc59872aa4bf716080bfee9efad402ba4d569
Sumeet Minhas <sumeetm273@gmail.com>
2017-03-30 03:20:42 +0000

    seed db for todos

---
+++ db/seeds/todos.js
@@ -0,0 +1,64 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('todos').del()
+    .then(function () {
+      return Promise.all([
+        knex('todos').insert({
+          rank: 1,
+          title: 'I want to eat at Meat & Bread',
+          completed: false,
+          deleted: false,
+          category_id: 3,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 2,
+          title: 'I want to eat at Tacofino',
+          completed: false,
+          deleted: false,
+          category_id: 3,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 3,
+          title: 'I want to eat at Nuba',
+          completed: false,
+          deleted: false,
+          category_id: 3,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 4,
+          title: 'I want to eat at Caveman Cafe',
+          completed: false,
+          deleted: false,
+          category_id: 3,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 5,
+          title: 'I want to eat at Lighthouse Lab Cafe',
+          completed: false,
+          deleted: false,
+          category_id: 3,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 1,
+          title: 'I want to watch Logan',
+          completed: false,
+          deleted: false,
+          category_id: 1,
+          user_id: 1
+        }),
+        knex('todos').insert({
+          rank: 2,
+          title: 'I want to watch Inglorious Bastards',
+          completed: false,
+          deleted: false,
+          category_id: 1,
+          user_id: 1
+        })
+      ]);
+    });
+};
