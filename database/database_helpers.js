8d33c7e5b1e52942428e67d3d3ed12fe683eae7b
Sumeet Minhas <sumeetm273@gmail.com>
2017-03-31 02:47:14 +0000

    creating todos

--- database/database_helpers.js
+++ database/database_helpers.js
@@ -3,81 +3,84 @@
 module.exports = (db) => {
 
   const databaseHelpers = {};
-    databaseHelpers.selectAllTodo = (cb) => {
-      db
-      .select("*")
-      .from("todos")
-      .then((results) => {
-        cb(null, results);
-      })
-      .catch((err) => {
-        cb(err);
-      });
-    };
-    databaseHelpers.selectCatTodo = (user_id, category_id, cb) => {
-      db("todos")
-      .select("*")
-      .where({
+
+  databaseHelpers.selectAllTodo = (cb) => {
+    db
+    .select("*")
+    .from("todos")
+    .then((results) => {
+      cb(null, results);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+  };
+
+  databaseHelpers.selectCatTodo = (user_id, category_id, cb) => {
+    db("todos")
+    .select("*")
+    .where({
+      user_id: user_id,
+      category_id: category_id
+    })
+    .then((results) => {
+      cb(null, results);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+  };
+
+  databaseHelpers.updateTodo = (updateColumn, todoID, cb) => {
+    db("todos").where("id", "=", todoID)
+    .update(updateColumn)
+    .then((results) => {
+      cb(null, results);
+    }).catch((err) => {
+      cb(err);
+    });
+  };
+
+
+  databaseHelpers.multiRankUpdate = (todoArr, cb) => {
+    return Promise.all(todoArr.map((todo) => {
+      return this.updateTodo({"rank": todo.rank}, todo.id, cb);
+    }))
+    .then((results) => {
+      cb(null, results);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+  };
+
+  databaseHelpers.createTodo = (user_id, category_id, title, cb) => {
+    db("todos").max("rank").where({
         user_id: user_id,
         category_id: category_id
       })
-      .then((results) => {
-        cb(null, results);
-      })
-      .catch((err) => {
-        cb(err);
-      });
-    };
-
-    databaseHelpers.updateTodo = (updateColumn, todoID, cb) => {
-      db("todos").where("id", "=", todoID)
-      .update(updateColumn)
-      .then((results) => {
-        cb(null, results);
-      }).catch((err) => {
-        cb(err);
-      });
-    };
-
-
-    databaseHelpers.multiRankUpdate = (todoArr, cb) => {
-      return Promise.all(todoArr.map((todo) => {
-        return this.updateTodo({"rank": todo.rank}, todo.id, cb);
-      }))
-      .then((results) => {
-        cb(null, results);
-      })
-      .catch((err) => {
-        cb(err);
-      });
-    };
-
-    databaseHelpers.createTodo = () => {
-      db("todos").max("rank").where({
-          user_id: 1,
-          category_id: 3
-        })
-    .then((val)=> {
-      var valToUse = val[0].max? val[0].max + 1 : 1;
+    .then((results)=> {
+      const rankToUse = results[0].max ? results[0].max + 1 : 1;
       db("todos").insert({
-        rank: valToUse,
-        title: 'I want to eat at Subway',
+        rank: rankToUse,
+        title: title,
         completed: false,
         deleted: false,
-        category_id: 3,
-        user_id: 1
+        category_id: category_id,
+        user_id: user_id
       })
-      .then((val) => {
-          return res.send();
-
+      .then((results) => {
+          cb(null, results);
         })
       .catch((err) => {
         console.log("Error", err)
+        cb(err);
       });
-      }).catch((err) => {
-        console.log("Error", err)
+    }).catch((err) => {
+      console.log("Error", err)
+      cb(err);
     });
-    };
+  };
 
   return databaseHelpers;
 
