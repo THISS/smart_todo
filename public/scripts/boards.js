ae8b08054e9e7e26205bfbfd62d5f6ba541a0e02
Jefeated <jeffng@live.ca>
2017-03-31 01:48:38 +0000

    finished header layout

--- public/scripts/boards.js
+++ public/scripts/boards.js
@@ -6,16 +6,16 @@ function addCard(e) {
     var lastId = items.firstChild.id;
     var id = items.children.length;
     $(count).text(id);
-    var hasComment = (Math.random() >= 0.5) ? '<a href="#"><i class="fa fa-comment-o"></i></a>' : '';
-    var hasCalendar = (Math.random() >= 0.5) ? '<a href="#"><i class="fa fa-calendar"></i></a>' : '';
+    // var hasComment = (Math.random() >= 0.5) ? '<a href="#"><i class="fa fa-comment-o"></i></a>' : '';
+    // var hasCalendar = (Math.random() >= 0.5) ? '<a href="#"><i class="fa fa-calendar"></i></a>' : '';
     var tmpl = '<div class="box" id="'+category+'c'+id+'">' +
     '<p class="title">'+ faker.lorem.sentence() +'</p>' +
-    '<p class="meta">' +
-    '<img src="https://placehold.it/28x28">' +
-    '<a href="#"><i class="fa fa-bars"></i></a>' +
-    hasComment +
-    hasCalendar +
-    '</p>' +
+    // '<p class="meta">' +
+    // '<img src="https://placehold.it/28x28">' +
+    // '<a href="#"><i class="fa fa-bars"></i></a>' +
+    // hasComment +
+    // hasCalendar +
+    // '</p>' +
     '</div>';
     $(items).prepend(tmpl);
 }
