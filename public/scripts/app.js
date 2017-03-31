46df7e9bed24ff2fbe54f5cb9c53c76c829e0867
Jefeated <jeffng@live.ca>
2017-03-31 21:12:33 +0000

    made functions for textbox

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+$(function(){
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -7,4 +9,49 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });
+});
+
+function loadtodos(){
+  $.ajax({
+    method: 'GET',
+    url: '/',
+    success: (data) => rendertodo(data)
+  });
+};
+
+$('#what-todo-box').on('submit', function (event) {
+    event.preventDefault();
+    var text = $('##what-todo-box text');
+
+    if(validateForm(text)) {
+      $.ajax({
+        method: 'POST',
+        url:'/',
+        data: text.serialize(),
+        success: function(){
+  
+          loadtweets();
+          text.val('');
+        }
+      });
+    }
+});
+
+function validateForm(text) {
+  if ($.trim(text.val()).length === 0) {
+    alert ("Can not sumbit an empty item");
+    return false;
+  }
+  else if (text.val().length > 255 ) {
+    alert ("Exceeded character limit of 255");
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+
+
+
 });
