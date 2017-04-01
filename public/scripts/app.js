e045a3bf06225017d4241f0321f1875b42f6bb77
Brenton Holswich <brenton.holswich@gmail.com>
2017-04-01 16:48:28 -0700

    separating functions so there are ajax and logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,10 +6,14 @@ $(function(){
 
   // Handlebars Joy
   // TODO: update the source html to a jQuery .html() of the script tags on the index
-  const sourceTodoHtml = `<li data-id="{{id}}" data-rank="{{rank}}"><label><input type="checkbox" checked="{{completed}}">{{title}}</label></li>`;
-  const sourceCatHtml = `<section><header><h3>{{name}}</h3><h3 class="cat-number"></h3></header><ul class="todo-wrapper"></ul></section>`;
+  const sourceTodoHtml = `<li data-id="{{id}}" data-rank="{{rank}}"><label><input type="checkbox" {{#if completed}}checked {{/if}} >{{title}}</label></li>`;
+  const sourceCatHtml = `<section data-id="{{id}}"><header><h3>{{name}}</h3><h3 class="cat-number"></h3></header><ul class="todo-wrapper"></ul></section>`;
   const templateTodo = Handlebars.compile(sourceTodoHtml);
   const templateCat = Handlebars.compile(sourceCatHtml);
+
+/*********************************************************************************/
+/*********************** the main todo renderers *********************************/
+/*********************************************************************************/
 
   // make a function that renders one TODO
   function todoMaker(todo) {
@@ -39,41 +43,84 @@ $(function(){
       // render a todo
       let todoHtml = todoMaker(todo);
       // append the todo to the category by id #cat_<category_id> use the categoryObj to do it
-      console.log(todoHtml);
-      console.log($(categoryObj[todo.category_id]).find(".todo-wrapper"));
-      // $(categoryObj[todo.category_id]).find(".todo-wrapper").append(todoHtml);
+      const categoryUl = categoryObj[todo.category_id].find("ul");
+      $(todoHtml).appendTo(categoryUl);
     });
     countTodos();
   }
 
   // make a function that loops over array of categories and sends them to categoryMaker
-  function categoryLooper(categoryArr, todoArr, cb) {
+  function categoryLooper(categoryArr) {
     // sort arrays by category id first if they aren't already 
-    $("section.categories").html(categoryArr.map((category) => {
-      const catHtml = categoryMaker(category);
-      categoryObj[category.id] = catHtml;
-      return catHtml;
-    }));
-    cb(todoArr);
+    $("section.categories").html(categoryArr.map(categoryMaker));
   }
 
-  // make a function that gets todos and categories and is the controller
-  function todoController(){
+/********************************************************************************/
+/*********************** All our Ajax functions *********************************/
+/********************************************************************************/
+
+  function getAllTodosAndCategories(errCb, successCb) {
     $.ajax({
       method: 'GET',
       url: '/todos',
-      success: function (data){
-        categoryLooper(data.categories, data.todos, todoLooper);
-      },
-      fail: function(err) {
-        errorFlash.text(err.error);
-      }
+      success: successCb,
+      fail: errCb
     });
   }
+
+  function updateCategory() {
+    // TODO: the PUT to change a todos category
+  }
+
+
+/******************************************************************************/
+/*********************** Our helper functions *********************************/
+/******************************************************************************/
+
+  // make a function that gets todos and categories and is the controller
+  function startController(){
+    getAllTodosAndCategories((err) => {
+      errorFlash.text(err.error);
+    }, (data) => {
+        categoryLooper(data.categories);
+        populateCategoryObj(data.categories);
+        todoLooper(data.todos);
+    });
+  }
+
+  function populateCategoryObj(categories) {
+    categories.forEach((category) => {
+      categoryObj[category.id] = $(`.categories section[data-id="${category.id}"]`);
+    }); 
+  }
+
+  function countTodos() {
+    // count the todo elements in each category and change the number at top right to the count
+    const catNumber = $(".cat-number");
+    catNumber.each(function(){
+      let count = $(this).closest("section").find(".todo-wrapper").children().length;
+      $(this).text(count);
+    });
+  }
+
+
+  function addedTodo(todo) {
+    // TODO: if todo.conflict is true render the select category page
+    // TODO: otherwise:
+    // change the categories number of todos
+    countTodos();
+    // TODO: if there is less than 3 todos render the todo to the category
+  }
+
+  // TODO: make a function that will remove the click handlers to submit PUT /todos/:todoId/category
+  // TODO: and then does an ajax call to PUT /todos/:todoId/category passing the category_id in the body which on success will:
+  // TODO:  - add the todos to the categories by calling todoLooper()
+  // TODO: and then adds a click handler to expand to single category mode on all of the categories
 
 /******************************************************************************/
 /*********************** Form submitting todo *********************************/
 /******************************************************************************/
+
   function validateForm(text) {
     if ($.trim(text.val()).length === 0) {
       // TODO: Change to a pretty flash message
@@ -88,33 +135,6 @@ $(function(){
     else {
       return true;
     }
-  }
-
-  function countTodos() {
-    // count the todo elements in each category and change the number at top right to the count
-    const catNumber = $(".cat-number");
-    catNumber.each(() => {
-      let count = $(this).closest("section").find(".todo-wrapper").children().length;
-      $(this).text(count);
-    });
-  }
-
-  // TODO: make a function that will remove the click handlers to expand to single category mode on all of the categories
-  // TODO: and then removes todos from the categories
-  // TODO: and then adds a click handler to submit the PUT /todos/:todoId/category
-  // TODO: this function will be added to all categories when they are made
-
-  // TODO: make a function that will remove the click handlers to submit PUT /todos/:todoId/category
-  // TODO: and then does an ajax call to PUT /todos/:todoId/category passing the category_id in the body which on success will:
-  // TODO:  - add the todos to the categories by calling todoLooper()
-  // TODO: and then adds a click handler to expand to single category mode on all of the categories
-
-  function addedTodo(todo) {
-    // TODO: if todo.conflict is true render the select category page
-    // TODO: otherwise:
-    // change the categories number of todos
-    countTodos();
-    // TODO: if there is less than 3 todos render the todo to the category
   }
 
   var text = $('#what-todo-box');
@@ -135,7 +155,36 @@ $(function(){
       }
   });
 
-  // Called at start
-  todoController();
+/******************************************************************************/
+/*********************** Render the dif pages *********************************/
+/******************************************************************************/
+
+  // make a function that will remove the click handlers to expand to single category mode on all of the categories
+  function renderSelectCategoryPage(event) {
+    const catSection = $(".categories section");
+    catSection.off();
+    catSection.find("ul").html("");
+    catSection.on("click", updateCategory);
+  }
+
+  function renderCategoryFocusPage(event) {
+    const catSection = $(".categories section");
+    $(this).off("click", renderCategoryFocusPage);
+    // TODO: add / remove the classes that make it a single category page
+    // TODO: maybe have a button to renderAllCategories again
+  }
+
+  function renderAllCategories(event) {
+    const catSection = $(".categories section");
+    catSection.off();
+
+    catSection.on("click", renderCategoryFocus);
+  }
+
+/******************************************************************************/
+/*********************** When ready call this *********************************/
+/******************************************************************************/
+
+  startController();
 
 });
