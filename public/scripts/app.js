8d2115c3b5145d5345945c6c8e10f733e88e57a7
Jefeated <jeffng@live.ca>
2017-03-31 20:55:25 -0700

    added log functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,57 @@
 $(function(){
 
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  function renderWhatTodos(data) {
+      for(i=0; i < data.length; i++){
+        // TODO: handlebars js - 
+        $('#todos').append(todo.title[i]);
+      }
+  }
+
+  function loadWhatTodos(){
+    $.ajax({
+      method: 'GET',
+      url: '/todos',
+      success: (data) => renderWhatTodos(data)
+    });
+  }
+
+  function validateForm(text) {
+    if ($.trim(text.val()).length === 0) {
+      // TODO: Change to a pretty flash message
+      alert ("Can not sumbit an empty item");
+      return false;
     }
+    else if (text.val().length > 127 ) {
+      // TODO: Change to a pretty flash message
+      alert ("Exceeded character limit of 127");
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  // TODO: Make a category flash function that we can call with a category on success of a todo submission
+  // If a category is not found then we need to go to pick category page / state
+
+  $('#what-todo-box').on('submit', function (event) {
+      event.preventDefault();
+      var text = $('#what-todo-box').val();
+
+      if(validateForm(text)) {
+        $.ajax({
+          method: 'POST',
+          url:'/todos',
+          data: text.serialize(),
+          success: function(){
+            loadWhatTodos();
+            text.val('');
+          }
+        });
+      }
   });
-});
 
-function loadWhatTodos(){
-  $.ajax({
-    method: 'GET',
-    url: '/todos',
-    success: (data) => renderWhatTodos(data)
-  });
-};
 
-$('#what-todo-box').on('submit', function (event) {
-    event.preventDefault();
-    var text = $('#what-todo-box text');
-
-    if(validateForm(text)) {
-      $.ajax({
-        method: 'POST',
-        url:'/',
-        data: text.serialize(),
-        success: function(){
-          loadWhatTodos();
-          text.val('');
-        }
-      });
-    }
-});
-
-function validateForm(text) {
-  if ($.trim(text.val()).length === 0) {
-    alert ("Can not sumbit an empty item");
-    return false;
-  }
-  else if (text.val().length > 255 ) {
-    alert ("Exceeded character limit of 255");
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-
-function renderWhatTodos(data) {
-    for(i=0; i<todos.length; i++){
-      $('#todos').append(todo.title[i]);
-    }
-}
 
 
 });
